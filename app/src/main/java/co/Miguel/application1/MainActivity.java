98f4358b55fcea4b9fb72a8c07653bdd1bf1d61c
Miguel Ángel Galeano <113398998+ElMiGuElG@users.noreply.github.com>
2024-02-06 09:11:09 -0500

    Add Variables
    New Variables

--- app/src/main/java/co/Miguel/application1/MainActivity.java
+++ app/src/main/java/co/Miguel/application1/MainActivity.java
@@ -5,7 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    String name = "Bob";
+    int num1 = 10;   // positive value
+    int num2 = -5;   // negative value
+    int num3 = 0;    // zero value
+    int age = 28;
+    char grade = 'A';
 
+    boolean late = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
