9e2540f4f5114eda0526ffa152e41518851a2f20
Miguel Ángel Galeano <113398998+ElMiGuElG@users.noreply.github.com>
2024-02-08 09:28:38 -0500

    Clase 2

--- app/src/main/java/co/Miguel/application1/MainActivity.java
+++ app/src/main/java/co/Miguel/application1/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
     String name = "Bob";
     int num1 = 10;   // positive value
     int num2 = -5;   // negative value
@@ -12,10 +13,37 @@ public class MainActivity extends AppCompatActivity {
     int age = 28;
     char grade = 'A';
 
+    TextView nombreUser;
+    Button btn1,btn2;
+
+
     boolean late = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombreUser = (TextView) findViewById(R.id.usuario);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                nombreUser.setText("Soy Boton 1");
+                
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                nombreUser.setText("Soy Boton 2");
+            }
+        });
+    }
+
+    public double CalcularArea(int l1, int l2){
+        return l1*l2;
     }
 }
