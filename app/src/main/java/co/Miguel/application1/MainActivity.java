699f4dd73adbca94396559af0413f7f46affa4a8
Miguel Ángel Galeano <113398998+ElMiGuElG@users.noreply.github.com>
2024-02-13 09:41:38 -0500

    Ciclo de Vida

--- app/src/main/java/co/Miguel/application1/MainActivity.java
+++ app/src/main/java/co/Miguel/application1/MainActivity.java
@@ -3,6 +3,7 @@ package co.Miguel.application1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,36 +18,82 @@ public class MainActivity extends AppCompatActivity {
     char grade = 'A';
 
     TextView nombreUser;
-    Button btn1,btn2;
-
+    Button btn1, btn2;
+    String Tag = "Prueba";
 
     boolean late = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nombreUser = (TextView) findViewById(R.id.Usuario);
+        nombreUser.setText("Estoy en Oncreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Tag, "Estoy en OnStart");
         setContentView(R.layout.activity_main);
 
         nombreUser = (TextView) findViewById(R.id.Usuario);
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
 
-        btn1.setOnClickListener(new View.OnClickListener(){
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 nombreUser.setText("Soy Boton 1");
-                
             }
         });
 
-        btn2.setOnClickListener(new View.OnClickListener(){
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 nombreUser.setText("Soy Boton 2");
             }
         });
+
     }
 
-    public double CalcularArea(int l1, int l2){
-        return l1*l2;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_main);
+        nombreUser = (TextView) findViewById(R.id.Usuario);
+        nombreUser.setText("Estoy en OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(Tag, "Estoy en OnPause");
+        setContentView(R.layout.activity_main);
+        nombreUser = (TextView) findViewById(R.id.Usuario);
+        nombreUser.setText("Estoy en OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(Tag, "Estoy en OnStop");
+        setContentView(R.layout.activity_main);
+        nombreUser = (TextView) findViewById(R.id.Usuario);
+        nombreUser.setText("Estoy en OnStop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(Tag, "Estoy en OnDestroy");
+        setContentView(R.layout.activity_main);
+        nombreUser = (TextView) findViewById(R.id.Usuario);
+        nombreUser.setText("Estoy en OnDestroy");
+    }
+
+    public double CalcularArea(int l1, int l2) {
+        return l1 * l2;
     }
 }
